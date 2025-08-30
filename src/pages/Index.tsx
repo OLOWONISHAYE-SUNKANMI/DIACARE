e2d97486072539e8e261ce5b9be56698df07aad2
DEV_BUDDY <allishittuabdulhameed@gmail.com>
2025-08-30 17:51:56 +0100

    tab nav

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -302,13 +302,13 @@ const Index = () => {
           {renderScreen()}
         </div>
 
-        {/* <Button
+        <Button
           onClick={handleQuickAdd}
           className="absolute bottom-20 right-0 w-14 h-14 rounded-full bg-medical-teal hover:bg-medical-teal/90 shadow-xl hover:shadow-2xl transition-all duration-200 hover:scale-110 z-50 ring-4 ring-medical-teal/20"
           size="icon"
         >
           <Plus className="w-6 h-6" />
-        </Button> */}
+        </Button>
         {/* ✅ Fixed Bottom Navigation */}
         
         {activeTab !== 'payment' && (
